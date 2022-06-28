dd5e7b74b7a7ecd80ef1c3ae5892f9478bd24bb4
journey3510 <1426173674@qq.com>
2022-06-28 14:42:42 +0800

    1

---
+++ src/app.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app">
+    <!-- <div class="app-info">Hello {{ AppName }} v{{ AppVersion }} !</div> -->
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+import { AppName, AppVersion } from './config'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  data() {
+    return {
+      AppName,
+      AppVersion,
+      active: 'home'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.app-info {
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(178, 94, 239, 0.5);
+  padding: 10px;
+  z-index: 999;
+}
+</style>
