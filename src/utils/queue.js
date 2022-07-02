8033bd0501cc43c124231a6ae53d17286215af0b
journey3510 <journey3510@gmail.com>
2022-07-02 23:58:10 +0800

    初步完成下载页基础动作

--- src/utils/queue.js
+++ src/utils/queue.js
@@ -8,6 +8,7 @@ export default class Queue {
   constructor(workerLen) {
     this.workerLen = workerLen || 3 // 同时执行的任务数
     this.list = [] // 任务队列
+    this.workeredList = [] // 已完成的任务
     this.statu = 1 // 执行状态
     this.worker = new Array(this.workerLen) // 正在执行的任务
   }
@@ -27,14 +28,18 @@ export default class Queue {
      * @param { Function } fn: 执行的函数
      * @param { Array<any> } args: 传递给执行函数的参数
      */
-  *
-  executionFunc(index, fn, ...args) {
+  * executionFunc(index, fn, ...args) {
+    console.log('args: ', args)
     const _this = this
-    yield fn.call(...args)
+    yield this.downtest(index, args[1], args[3])
       .then(function() {
         // 任务执行完毕后，再次分配任务并执行任务
-        _this.worker[index] = undefined
-        _this.run()
+        _this.worker[index].xx = 100
+        setTimeout(() => {
+          _this.worker[index] = undefined
+          _this.workeredList.push(args[3])
+          _this.run()
+        }, 1000)
       })
   }
 
@@ -46,6 +51,24 @@ export default class Queue {
     for (const item of list) {
       this.list.unshift(item)
     }
+  }
+
+  downtest(id, time, name) {
+    return new Promise((resolve) => {
+      const _this = this
+      let thenum = 0
+
+      const pageTimer = setInterval(() => {
+        thenum = thenum + 0.1
+        _this.worker[id].xx = parseInt(thenum / time * 100)
+        console.log('_this.worker[{$id}].xx: ', _this.worker[id].xx)
+      }, 100)
+
+      setTimeout(() => {
+        clearInterval(pageTimer)
+        resolve()
+      }, (time + 1) * 1000)
+    })
   }
 
   // 分配并执行任务
@@ -60,8 +83,14 @@ export default class Queue {
 
       if (!this.worker[i] && len > 0) {
         // 需要执行的任务
-        this.worker[i] = this.executionFunc(i, ...this.list[len - 1])
-
+        const worker = {
+          name: this.list[len - 1][4],
+          number: 20,
+          time: this.list[len - 1][3],
+          xx: 4,
+          func: this.executionFunc(i, ...this.list[len - 1])
+        }
+        this.worker[i] = worker
         runIndex.push(i)
 
         // 从任务队列内删除任务
@@ -74,7 +103,7 @@ export default class Queue {
 
     // 执行任务
     for (const index of runIndex) {
-      this.worker[index].next()
+      this.worker[index].func.next()
     }
   }
 }
