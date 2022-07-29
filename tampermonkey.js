32235e3d8f0b64751302ba399be62f280f84ba6a
journey3510 <journey3510@gmail.com>
2022-07-29 16:10:15 +0800

    油猴v1.0.10  新增27漫画网

--- tampermonkey.js
+++ tampermonkey.js
@@ -2,7 +2,7 @@
 // @name         __APP_NAME__
 // @namespace    http://tampermonkey2.net/
 // @version      __APP_VERSION__
-// @description  漫画分章节压缩下载。适用于 武侠漫画、酷漫屋、百漫谷、动漫之家、七夕漫画、36漫画网、古风漫画网、腾讯漫画、漫画星球、好漫8、漫画屋
+// @description  漫画分章节压缩下载。适用于 酷漫屋、百漫谷、武侠漫画、动漫之家、七夕漫画、36漫画网、古风漫画网、腾讯漫画、漫画星球、好漫8、漫画屋、27漫画网
 // @author       journey3510
 // @run-at       document-end
 // @grant        GM_getValue
@@ -21,34 +21,50 @@
 // @require      https://cdn.bootcdn.net/ajax/libs/vue/2.6.12/vue.min.js
 // @require      https://cdn.bootcdn.net/ajax/libs/jszip/3.7.1/jszip.min.js
 // @require      https://unpkg.com/vant@2.12/lib/vant.min.js
-
+//
 // @match        *://www.kumw5.com/*
+// @connect      kumw5.com
+//
 // @match        *://darpou.com/*
+// @connect      manga8.xyz
+//
 // @match        *://m.wuxiamh.com/*
 // @match        *://www.wuxiamh.com/*
-// @match        *://manhua.dmzj.com/*
-// @match        *://qiximh1.com/*
-// @match        *://www.36manga.com/*
-// @match        *://www.gufengmanhua.com/*
-// @match        *://ac.qq.com/*
-// @match        *://www.mhxqiu1.com/*
-// @match        *://www.123gf.com/*
-// @match        *://www.haoman8.com/*
-// @match        *://www.mh5.org/*
-// @connect      kumw5.com
-// @connect      manga8.xyz
 // @connect      169gouwu.com
+//
+// @match        *://manhua.dmzj.com/*
 // @connect      dmzj.com
+//
+// @match        *://qiximh1.com/*
 // @connect      byteimg.com
-// @connect      arc-theday.com
+//
+// @match        *://www.36manga.com/*
+//
+// @match        *://www.gufengmanhua.com/*
 // @connect      gufengmanhua.com
+// @match        *://www.123gf.com/*
 // @connect      123gf.com
-// @connect      xiaoqinre.com
+//
+// @match        *://ac.qq.com/*
 // @connect      acimg.cn
+//
+// @match        *://www.mhxqiu1.com/*
 // @connect      byteimg.com
+//
+// @match        *://www.haoman8.com/*
 // @connect      haoman8.com
+//
+// @match        *://www.mh5.org/*
 // @connect      xiaomingtaiji.net
+//
+// @match        *://www.2mzx.com/*
+// @connect      2mzx.com
+// @connect      hnsrht.com
+// @connect      huayingrenren.cn
+//
 // @connect      *
+// @connect      arc-theday.com
+// @connect      xiaoqinre.com
 // @connect      laimidao.com
 // @connect      izhegu.com
 // @connect      nicefee.com
